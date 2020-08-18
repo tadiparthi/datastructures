5ac49f764aaac50e3e0ab0709c6c3f117ea94553
Hari Tadiparthi <hari.tadiparthi@gmail.com>
2020-08-18 12:38:40 -0700

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <utility>
+using namespace std;
+
+void
+solve(int id)
+{
+	cout <<"Case $" << id << ": " <<  endl;
+}
+
+int
+main(int argc, char **argv)
+{
+	int t;
+	cin >> t;
+	for(int i = 1; i <= t; i++) {
+		solve(i);
+	}
+}
